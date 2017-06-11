44ae4a5742a6128c31ddeda827f447ed867e905f
Sung J Byun <sungjbyun@D-108-179-166-253.dhcp4.washington.edu>
2017-06-10 21:16:47 -0700

    router 4.x is so sweet

--- app/Components/App/App.js
+++ app/Components/App/App.js
@@ -2,13 +2,32 @@
 
 import React, { Component } from 'react';
 import Popular from '../Popular/Popular';
+import Nav from '../Nav/Nav';
+import { Home } from '../Home/Home';
+import Battle from '../Battle/Battle';
+
+var ReactRouter = require('react-router-dom');
+
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 
 class App extends Component {
-    render() {
-        return (
-        <div className="container">
-          <Popular />
-        </div>
+  render() {
+    return (
+        <Router>
+          <div className="container">
+            <Nav />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/battle' component={Battle} />
+              <Route path='/popular' component={Popular} />
+              <Route render={ () => {
+                  return <h2>Not Found</h2>
+                  }} />
+            </Switch>
+          </div>
+        </Router>
         )
     }
 }
---
+++ app/Components/Battle/Battle.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+class Battle extends Component {
+  render() {
+    return (
+      <div>
+        Battle Ground
+      </div>
+    )
+  }
+}
+
+module.exports = Battle;
